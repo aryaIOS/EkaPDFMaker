44123abed0297b4468c261f51743e4125e211f42
aryaV <98746027+aryavashisht7844@users.noreply.github.com>
2024-10-23 16:20:31 +0530

    Make public init for pdf renderer

--- Sources/PDFMaker/Converters/PDFConverter.swift
+++ Sources/PDFMaker/Converters/PDFConverter.swift
@@ -22,6 +22,10 @@ public enum UrlPath {
 @MainActor
 @available(iOS 16.0, *)
 public struct PDFRenderer {
+  
+  public init() {
+    
+  }
   /// Used to render
   /// - Parameter view: The SwiftUI View that you want to be rendered
   /// - Parameter urlPathString:The path at which the pdf is to be updated
