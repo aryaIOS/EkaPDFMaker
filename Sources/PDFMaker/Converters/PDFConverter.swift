e18cda26bb9f53786970687de4d9e412714afc9a
aryaIOS <arya.vashisht@eka.care>
2025-03-26 09:56:47 +0530

    Scale factor multiplication

--- Sources/PDFMaker/Converters/PDFConverter.swift
+++ Sources/PDFMaker/Converters/PDFConverter.swift
@@ -227,7 +227,7 @@ public struct PDFRenderer {
 //    )
     let bodyRenderer = ImageRenderer(
       content: finalBodyView
-        .scaleEffect(x: 0.8, y: 0.8, anchor: .topLeading)
+        .scaleEffect(x: scaleFactor, y: scaleFactor, anchor: .topLeading)
     )
 
 //    if let consumer = CGDataConsumer(url: url as CFURL),
@@ -272,7 +272,7 @@ public struct PDFRenderer {
       
       // Flip the coordinate system to match typical view coordinates
       context.translateBy(x: 0, y: pageSize.height)
-//      context.scaleBy(x: 1, y: -1)
+      context.scaleBy(x: 1, y: -1)
       
 //      // Apply scaling transformation
 //      context.scaleBy(x: scaleFactor, y: scaleFactor)
