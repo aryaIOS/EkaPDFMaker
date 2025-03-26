751a8b6c67f633d74156e7c0d6fad82cc0746918
aryaIOS <arya.vashisht@eka.care>
2025-03-26 10:19:11 +0530

    Clipping fixed for bigger texts and header added

--- Sources/PDFMaker/Converters/PDFConverter.swift
+++ Sources/PDFMaker/Converters/PDFConverter.swift
@@ -212,37 +212,6 @@ public struct PDFRenderer {
         .scaleEffect(x: scaleFactor, y: scaleFactor, anchor: .bottomLeading)
     )
 
-//    if let consumer = CGDataConsumer(url: url as CFURL),
-//       let context = CGContext(consumer: consumer, mediaBox: nil, nil) {
-//      
-//      /// By default the media box renders items at the bottom
-//      var mediaBox = CGRect(origin: .zero, size: pageSize)
-//      context.beginPage(mediaBox: &mediaBox)
-//      /// Save the state
-//      context.saveGState()
-//      
-//      /// Transformation of the coordinate system
-//      /// Note however this will render the contents upside down
-//      /// You have to flip the views beforehand to ensure the coordinates are correct
-////      context.translateBy(x: 0, y: pageSize.height) /// Move the items up
-////      context.scaleBy(x: 1, y: -1) /// Flip the scale
-//      
-////      /// Render the header at the top
-////      headerRenderer.render { size, renderer in
-////        renderer(context)
-////      }
-////      
-////      context.translateBy(x: 0, y: headerHeight)
-//      
-//      bodyRenderer.render { size, renderer in
-//        renderer(context)
-//      }
-//      
-//      // Restore and close the PDF context
-//      context.restoreGState()
-//      context.endPDFPage()
-//      context.closePDF()
-//    }
     if let consumer = CGDataConsumer(url: url as CFURL),
        let context = CGContext(consumer: consumer, mediaBox: nil, nil) {
       
@@ -252,12 +221,12 @@ public struct PDFRenderer {
       context.beginPage(mediaBox: &mediaBox)
       context.saveGState()
       
-      // Flip the coordinate system to match typical view coordinates
-//      context.translateBy(x: 0, y: pageSize.height)
-//      context.scaleBy(x: 1, y: -1)
+      /// Render the header at the top
+      headerRenderer.render { size, renderer in
+        renderer(context)
+      }
       
-//      // Apply scaling transformation
-//      context.scaleBy(x: scaleFactor, y: scaleFactor)
+      context.translateBy(x: 0, y: headerHeight)
       
       bodyRenderer.render { size, renderer in
         renderer(context)
